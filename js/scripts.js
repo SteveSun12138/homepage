ce8132777bdac4f58a5872552ad8d920641f1f08
eqiowtj <113475124+eqiowtj@users.noreply.github.com>
2022-09-26 23:21:10 -0700

    fix js lint issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,116 @@
 /*!
-    * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
-    */
-    (function($) {
-    "use strict"; // Start of use strict
-  
-    // Smooth scrolling using jQuery easing
-    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: (target.offset().top - 71)
-          }, 1000, "easeInOutExpo");
-          return false;
-        }
+ * Start Bootstrap - Freelancer v6.0.0 (https://startbootstrap.com/themes/freelancer)
+ * Copyright 2013-2020 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-freelancer/blob/master/LICENSE)
+ */
+(function ($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ===
+        this.pathname.replace(/^\//, "") &&
+      location.hostname === this.hostname
+    ) {
+      let target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top - 71,
+          },
+          1000,
+          "easeInOutExpo"
+        );
+        return false;
       }
-    });
-  
-    // Scroll to top button appear
-    $(document).scroll(function() {
-      var scrollDistance = $(this).scrollTop();
-      if (scrollDistance > 100) {
-        $('.scroll-to-top').fadeIn();
-      } else {
-        $('.scroll-to-top').fadeOut();
-      }
-    });
-  
-    // Closes responsive menu when a scroll trigger link is clicked
-    $('.js-scroll-trigger').click(function() {
-      $('.navbar-collapse').collapse('hide');
-    });
-  
-    // Activate scrollspy to add active class to navbar items on scroll
-    $('body').scrollspy({
-      target: '#mainNav',
-      offset: 80
-    });
-  
-    // Collapse Navbar
-    var navbarCollapse = function() {
-      if ($("#mainNav").offset().top > 100) {
-        $("#mainNav").addClass("navbar-shrink");
-      } else {
-        $("#mainNav").removeClass("navbar-shrink");
-      }
-    };
-    // Collapse now if page is not at top
-    navbarCollapse();
-    // Collapse the navbar when page is scrolled
-    $(window).scroll(navbarCollapse);
-  
-    // Floating label headings for the contact form
-    $(function() {
-      $("body").on("input propertychange", ".floating-label-form-group", function(e) {
-        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-      }).on("focus", ".floating-label-form-group", function() {
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).scroll(function () {
+    const scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $(".scroll-to-top").fadeIn();
+    } else {
+      $(".scroll-to-top").fadeOut();
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $("body").scrollspy({
+    target: "#mainNav",
+    offset: 80,
+  });
+
+  // Collapse Navbar
+  const navbarCollapse = function () {
+    if ($("#mainNav").offset().top > 100) {
+      $("#mainNav").addClass("navbar-shrink");
+    } else {
+      $("#mainNav").removeClass("navbar-shrink");
+    }
+  };
+  // Collapse now if page is not at top
+  navbarCollapse();
+  // Collapse the navbar when page is scrolled
+  $(window).scroll(navbarCollapse);
+
+  // Floating label headings for the contact form
+  $(function () {
+    $("body")
+      .on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass(
+          "floating-label-form-group-with-value",
+          !!$(e.target).val()
+        );
+      })
+      .on("focus", ".floating-label-form-group", function () {
         $(this).addClass("floating-label-form-group-with-focus");
-      }).on("blur", ".floating-label-form-group", function() {
+      })
+      .on("blur", ".floating-label-form-group", function () {
         $(this).removeClass("floating-label-form-group-with-focus");
       });
-    });
-  
-  })(jQuery); // End of use strict
-  
-var timer;
-var ele = document.getElementById('timer');
+  });
+})(jQuery); // End of use strict
 
-(function (){
-  var sec = 0;
-  var min = 0;
-  timer = setInterval(()=>{
+const ele = document.getElementById("timer");
+
+(function () {
+  let sec = 0;
+  let min = 0;
+  setInterval(() => {
     if (min < 10) {
       if (sec < 10) {
-        ele.innerHTML = '0' + min + ':0' + sec;
+        ele.innerHTML = "0" + min + ":0" + sec;
       } else {
-        ele.innerHTML = '0' + min + ':' + sec;
+        ele.innerHTML = "0" + min + ":" + sec;
       }
-      if (sec == 59) {
+      if (sec === 59) {
         sec = 0;
         min++;
       }
     } else {
       if (sec < 10) {
-        ele.innerHTML = min + ':0' + sec;
+        ele.innerHTML = min + ":0" + sec;
       } else {
-        ele.innerHTML = min + ':' + sec;
+        ele.innerHTML = min + ":" + sec;
       }
-      if (sec == 59) {
+      if (sec === 59) {
         sec = 0;
         min++;
       }
     }
-    if (min == 59 && sec == 59) {
+    if (min === 59 && sec === 59) {
       min = 0;
       sec = 0;
     }
     sec++;
-  }, 1000)
-})()
+  }, 1000);
+})();
